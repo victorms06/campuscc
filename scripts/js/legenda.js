3c56d1cf528e3221b68f2463a2d75ab1fedc3ba5
victorms06 <victoralexandremachadodasilva@gmail.com>
2024-12-06 00:01:27 -0300

    Update project

--- scripts/js/legenda.js
+++ scripts/js/legenda.js
@@ -1,2 +1,21 @@
-const legend = document.getElementById('legend');
-map.getContainer().appendChild(legend);
+const legenda = document.getElementById('legend');
+const legendaBtn = document.getElementById('legendaBtn');
+map.getContainer().appendChild(legenda);
+map.getContainer().appendChild(legendaBtn);
+
+
+legendaBtn.addEventListener('click', () => {
+    legenda.classList.toggle('hidden'); // Alterna a visibilidade
+    if (legenda.classList.contains('hidden')) {
+        legendaBtn.textContent = 'Mostrar Legenda'; // Texto do botão
+    } else {
+        legendaBtn.style.display = 'none'; 
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (!legenda.contains(event.target) && event.target !== legendaBtn) {
+        legenda.classList.add('hidden'); // Fecha a legenda
+        legendaBtn.style.display = 'block'; 
+    }
+});
